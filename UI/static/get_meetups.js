709835fa8cb3cba2347494720541159ee8540fe3
AlvinMugambi <ngurual123@gmail.com>
2019-01-24 01:49:52 +0300

    [chore #163400205]switch url to use the heroku url

--- UI/static/get_meetups.js
+++ UI/static/get_meetups.js
@@ -5,7 +5,7 @@ function deleteMeetup(e) {
   var id = this.getAttribute('id');
 
   // DELETE
-  let url = `http://127.0.0.1:5000/api/v2/meetups/${id}`;
+  let url = `https://the-questioner-backend.herokuapp.com/api/v2/meetups/${id}`;
   fetch(url, {
     method : 'DELETE',
     headers : {
